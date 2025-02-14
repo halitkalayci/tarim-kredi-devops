5502a37368ddbfbd4289651ef15cfad73b0238ad
halitkalayci <halit.kalayci@ext.etiya.com>
2025-02-14 10:35:54 +0300

    add routes

--- ExampleClient/src/app/app.routes.ts
+++ ExampleClient/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'homepage'},
+  {path:'homepage', component:HomepageComponent},
+  {path:'about-us', component:AboutUsComponent}
+];
---
+++ ExampleClient/src/app/pages/about-us/about-us.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
